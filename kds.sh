83c0eb3f68aae2a78dded7a6965f29d96f8ab5f9
Geremy Cohen <geremy.cohen@arm.com>
2025-10-14 20:56:14 +0000

    kds add

--- kds.sh
+++ kds.sh
@@ -1,4 +1,6 @@
 ps -ef | grep -i bench ;ps -ef | grep -o python;sudo pkill -9 -f bench
 ./dual_installer.sh create delete
 ./dual_installer.sh create --nodes 16 --shards 16 --heap 80
+#rm -rf results/optimization/nyc_taxi_1014_full/
+
 
